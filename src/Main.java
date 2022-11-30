07613bb7bde50581fd95ca949d0356bc643cb7be
Macintosh <rassadinmisha714@gmail.com>
2022-11-30 16:29:03 +0300

    Задача 1

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,26 @@
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        task1();
+    }
+
+    public static void task1 () {
+        byte apple = 100;
+        System.out.println("Значение перемменой apple с типом byte равно " + apple);
+
+        short banana = 2000;
+        System.out.println("Значение перемменой banana с типом short равно " + banana);
+
+        int orange = 150000;
+        System.out.println("Значение перемменой orange с типом int равно " + orange);
+
+        long mandarin = 1000000;
+        System.out.println("Значение перемменой mandarin с типом long равно " + mandarin);
+
+        float cookie = 5f;
+        System.out.println("Значение перемменой cookie с типом float равно " + cookie);
+
+        double bone = 7.8;
+        System.out.println("Значение перемменой done с типом double равно " + bone);
     }
 }
